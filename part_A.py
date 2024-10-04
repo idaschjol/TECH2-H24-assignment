e85264b556252cfccc80052e081f9b5690ee4b04
Ida Schjølberg <idaschjolberg@Idas-MacBook-Pro.local>
2024-10-04 23:00:06 +0200

    Final Part A

--- part_A.py
+++ part_A.py
@@ -8,74 +8,54 @@ from math import sqrt
 import numpy as np
 
 
+#Defining a list with the given numbers:
+num_lst = [1, 2, 3, 4, 5]
+
+
 def std_loops(x):
-    N = len(num_lst)
+    L = len(x)  #Finding the length of the list
     mean = 0
-    for num in num_lst:
+    for num in x:  #First loop to calculate the mean
         mean += num
-    mean /= N
+    mean /= L
     
     
     variance = 0
-    for num in num_lst:
+    for num in x:   #Second loop to calculate the variance
         variance += (num-mean)**2
-    variance /= N
+    variance /= L
     
     
-    std = sqrt(variance)
+    std = sqrt(variance)  #Calcualting the standard deviation by taking the square root of the variance
     return std
     
-    """
-    Compute standard deviation of x using loops.
-
-    Parameters
-    ----------
-    x: Sequence of numbers
-
-    Returns
-    -------
-    sd : float
-        Standard deviation of the list of numbers.
-    """
-      
-
+    
+    
+#Doing the same by just using sum() and len()
 def std_builtin(x):
-    N = len(num_lst)
-    mean = sum(num_lst)/N
-    variance = sum((x-mean)**2 for x in num_lst)/N 
+    L = len(x)
+    mean = sum(x)/L
+    variance = sum((x-mean)**2 for x in x)/L 
     std = sqrt(variance)
     return std
 
 
-    """
-    Compute standard deviation of x using the built-in functions sum()
-    and len().
 
-    Parameters
-    ----------
-    x: Sequence of numbers
-
-    Returns
-    -------
-    sd : float
-        Standard deviation of the list of numbers.
-    """
-    
-   
-def std_numpy(num_lst):
-    return np.std(num_lst)
+#Using numpy to calculate:   
+def std_numpy(x):
+    return np.std(x)
 
 
-num_lst = [1, 2, 3, 4, 5]
-
+#Calculating the standard deviation for all the three methods:
 std1 = std_loops(num_lst)
 std2 = std_builtin(num_lst)
 std3 = std_numpy(num_lst)
 
 
-print(f"Standard Deviation (Loop): {std1}")
-print(f"Standard Deviation (Built-in): {std2}")
-print(f"Standard Deviation (NumPy): {std3}")
+#Printing the results:
+print(f"Standard Deviation by using loops is: {std1}")
+print(f"Standard Deviation by using built-in functions is: {std2}")
+print(f"Standard Deviation by using NumPy is: {std3}")
 
 
 
